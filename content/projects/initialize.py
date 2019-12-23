8acc61176cab78a5f2406bafc15d9218d65ab9a1
Syed Hasan <hasansi@gmail.com>
2019-12-23 22:47:22 +0000

    changes

--- content/projects/initialize.py
+++
@@ -1,6 +0,0 @@
-import findspark
-findspark.init('/home/ubuntu/spark-2.1.1-bin-hadoop2.7')
-import pyspark
-from pyspark.sql import SparkSession
-app_name='app'
-spark=SparkSession.builder.appName(app_name).getOrCreate()
