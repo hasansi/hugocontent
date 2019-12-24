d849a32dca96fb4e61b27615ead3190a06357110
Syed Hasan <hasansi@gmail.com>
2019-12-24 01:29:54 +0000

    changes

--- deploy.sh
+++ deploy.sh
@@ -1,33 +1,5 @@
 #!/bin/sh
 
-# If a command fails then the deploy stops
-set -e
+cd ~/hugocontent && git add . && git commit -m "changes" && git push origin master && hugo -d ~/hasansi.github.io
 
-printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
-
-# Build the project.
-#hugo -t coder
-
-# Go To hugo folder
-cd ~/hugocontent
-
-# Add changes to git.
-git add .
-
-# Commit changes.
-msg="rebuilding site $(date)"
-if [ -n "$*" ]; then
-		msg="$*"
-	fi
-	git commit -m "$msg"
-
-	# Push source and build repos.
-	git push origin master
-
-
-hugo -d ~/hasansi.github.io
-
-	
-# Go To username folder
 cd ~/hasansi.github.io && git add . && git commit -m "changes" && git push origin master
-cd ~/
